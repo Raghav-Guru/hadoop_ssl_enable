7708f4e4a33785664ed624f1e8e0723464a800d5
rguruvan <rguruvan@gmail.com>
2018-03-18 12:43:52 +0530

    Update ssl_setup_hdp.py

--- ssl_setup_hdp.py
+++ ssl_setup_hdp.py
@@ -5,15 +5,15 @@ import sys
 import json
 import socket
 import time
-import optparse
-from optparse import OptionGroup
-import xml
-import xml.etree.ElementTree as ET
 import logging
 import ConfigParser
 
 config= ConfigParser.ConfigParser()
 config.read('config.ini')
+
+logging.addLevelName(logging.WARNING, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName(logging.INFO, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(message)s')
 
 AMBARI_USER_ID=config.get('configuration','ambari_user')
 AMBARI_USER_PW=config.get('configuration','ambari_user_password')
@@ -27,7 +27,6 @@ ssl_script=''.join([dirpath,'/ssl_create.sh'])
 host_file=''.join([dirpath,'/hosts.txt'])
 protocol = 'http'
 
-logger = logging.getLogger('AmbariConfig')
 HTTP_PROTOCOL = 'http'
 HTTPS_PROTOCOL = 'https'
 SET_ACTION = 'set'
@@ -43,91 +42,79 @@ TAG_PREFIX = 'version'
 GET_REQUEST_TYPE = 'GET'
 
 
+logging.info('CREATING ssl_create script %s' %(ssl_script))
 
 #####Create shell script to be executed on Ambari server##
 with open("ssl_create.sh","w") as w :
      w.write("""\
 #!/bin/bash
+
+if [ $1 == "setup" ]
+then
 mkdir -p /var/lib/ambari-server/resources/ssl_setup
 AMBARI_SERVER=`hostname -f`
 
-#netstat -tn 2>/dev/null | grep ':8441' | awk '{print $5}' |sed -e 's/::ffff://' | cut -f1 -d: | uniq | sort -rn >/tmp/hosts.txt
-#curl -s -u admin:admin http://$AMBARI_SERVER:8080/api/v1/clusters/seclab/hosts | grep host_name | awk -F':' '{print $2}' | tr -d '\"' > /tmp/hosts.txt
-
 for i in `cat /tmp/hosts.txt`
 do
-keytool -genkey -noprompt -alias server -dname "CN=$i, OU=support, O=HWX, L=Bengaluru, S=Karnataka, C=IN" -keystore /var/lib/ambari-server/resources/ssl_setup/$i.jks -storepass changeit -keypass changeit -validity 365
+keytool -genkey -keyalg rsa -keysize 2048 -noprompt -alias server -dname "CN=$i, OU=support, O=HWX, L=SantaClara, S=CA, C=US" -keystore /var/lib/ambari-server/resources/ssl_setup/$i.jks -storepass changeit -keypass changeit -validity 365
 
 #cp /etc/security/serverKeys/$i.jks /etc/security/clientKeys/
 
 keytool -noprompt -export -file /var/lib/ambari-server/resources/ssl_setup/$i.pem -keystore /var/lib/ambari-server/resources/ssl_setup/$i.jks -alias server -storepass changeit
 keytool -noprompt -import -file /var/lib/ambari-server/resources/ssl_setup/$i.pem -keystore /var/lib/ambari-server/resources/ssl_setup/all.jks -storepass changeit -alias $i
 done
-#CONTENT="<actionDefinition>\\n<actionName>ssl_setup</actionName>\\n<actionType>USER</actionType>\\n</inputs>\\n<targetService/>\\n<targetComponent/>\\n<defaultTimeout>60</defaultTimeout>\\n<description>Copy Keystore to ALl agents</description>\\n<targetType>ALL</targetType>\\n</actionDefinition>"
-CONTENT="<actionDefinition>\\n<actionName>ssl_setup</actionName>\\n<actionType>SYSTEM</actionType>\\n<inputs/>\\n<targetService/>\\n<targetComponent/>\\n<defaultTimeout>60</defaultTimeout>\\n<description>Copy Keystore to ALl agents</description>\\n<targetType>ALL</targetType>\\n<permissions>HOST.ADD_DELETE_COMPONENTS, HOST.ADD_DELETE_HOSTS, SERVICE.ADD_DELETE_SERVICES</permissions>\\n</actionDefinition>"
-C=$(echo $CONTENT | sed 's/\//\\\\\//g')
-
-sed -i.bak "/<\/actionDefinitions>/ s/.*/${C}\\n&/" /var/lib/ambari-server/resources/custom_action_definitions/system_action_definitions.xml
-
-#### setup truststore on Ambari-server ###
-
-cp /var/lib/ambari-server/resources/ssl_setup/all.jks /etc/ambari-server/conf/ambari-truststore.jks
-#ambari-server setup-security --security-option=setup-truststore --truststore-type=jks --truststore-path=/etc/ambari-server/conf/ambari-truststore.jks --truststore-password=changeit --truststore-reconfigure
-
-echo "Restarting Ambari Server"
-
-ambari-server stop
-sleep 5
-
-ambari-server start
-
-listen=1
-if [ listen == 1 ]
-then
-  echo "Waiting for port 8080"
-  netstat -an | grep ":8080"
-  listen=`echo $?`
-fi
-###Restore system_action_definitions after restart
-
-#cp /var/lib/ambari-server/resources/custom_action_definitions/system_action_definitions.xml.bak /var/lib/ambari-server/resources/custom_action_definitions/system_action_definitions.xml
 
 ##Start python SimpleHTTPServer 
-cd /var/lib/ambari-server/resources/ssl_setup
+cd /var/lib/ambari-server/resources/
 python -m SimpleHTTPServer 8000 &> /dev/null &
-echo $! > /tmp/http.pid
-#kill -9 `cat /tmp/http.pid`
+ps -ef | grep SimpleHTTPServer | grep -v grep | awk -F' ' '{print $2}' > /tmp/SimpleHTTPServer.pid
+fi
+
+if [ $1 == "kill" ]
+then
+kill -9 `cat /tmp/SimpleHTTPServer.pid`
+fi 
+
+if [ $1 == "setup-truststore" ]
+then
+cp /var/lib/ambari-server/resources/ssl_setup/all.jks /etc/ambari-server/conf/ambari-truststore.jks
+ambari-server setup-security --security-option=setup-truststore --truststore-type=jks --truststore-path=/etc/ambari-server/conf/ambari-truststore.jks --truststore-password=changeit --truststore-reconfigure
+echo "Restarting Ambari Server"
+ambari-server restart
+fi
 """)
 os.chmod("ssl_create.sh",0755)
 
-###Create Custom action script for Ambari for keystore copy task##
+###Create Custom action script for Ambari keystore copy operation##
 
-with open("ssl_setup.py","w") as w :
+with open("validate_configs.py","w") as w :
      w.write("""\
 import urllib
 import os
 import shutil
 import subprocess
-import socket
 import ConfigParser
+import time
 
 subprocess.call(['mkdir', '-p', '/etc/security/clientKeys/'])
 subprocess.call(['mkdir', '-p', '/etc/security/serverKeys/'])
+
 certfile=socket.gethostname()
 config= ConfigParser.ConfigParser()
 config.read('/etc/ambari-agent/conf/ambari-agent.ini')
 ambari_server=config.get('server','hostname')
-#url1="http://%s:8080/resources/ssl_setup/%s.jks" % (ambari_server,certfile)
-#url2="http://%s:8080/resources/ssl_setup/all.jks" % (ambari_server)
 
-url1="http://%s:8000/%s.jks" % (ambari_server,certfile)
-url2="http://%s:8000/all.jks" % (ambari_server)
+url1="http://%s:8000/ssl_setup/%s.jks" % (ambari_server,certfile)
+url2="http://%s:8000/ssl_setup/all.jks" % (ambari_server)
+url3="http://%s:8000/custom_actions/scripts/validate_configs.py" % (ambari_server)
 
 urllib.urlretrieve (url1, "/etc/security/serverKeys/keystore.jks")
 urllib.urlretrieve (url2, "/etc/security/clientKeys/all.jks")
 shutil.copy2('/etc/security/serverKeys/keystore.jks', '/etc/security/clientKeys/keystore.jks')
+time.sleep(2)
+urllib.urlretrieve (url3, "/var/lib/ambari-agent/cache/custom_actions/scripts/validate_configs.py")
 """)
-os.chmod("ssl_setup.py",0755)
+os.chmod("validate_configs.py",0755)
 
 def ambariREST( restAPI ) :
     url="http://"+AMBARI_DOMAIN+":"+str(AMBARI_PORT)+restAPI
@@ -172,7 +159,6 @@ def getMRHistory():
     restAPI = CLUSTER_API+"/services/MAPREDUCE2/components/HISTORYSERVER"
     json_data =  ambariREST(restAPI)
     history_server=json_data['host_components'][0]['HostRoles']['host_name']
-    #print history_server['host_components'][0]['HostRoles']['host_name']
     return history_server
     
 def sshAmbariAgents() : 
@@ -180,46 +166,36 @@ def sshAmbariAgents() :
     json_data =  ambariREST(restAPI)
     for index in range(len(json_data['items'])):
         host_ip=json_data['items'] [index] ['Hosts'] ['ip']
-        scp1=''.join(['scp', ' ', '-i', PEM_FILE, ' ','ssl_setup.py', ' ', 'root', '@', host_ip, ':', '/var/lib/ambari-agent/cache/custom_actions/scripts'])
+        scp1=''.join(['scp', ' ', '-i', PEM_FILE, ' ','validate_configs.py', ' ', 'root', '@', host_ip, ':', '/var/lib/ambari-agent/cache/custom_actions/scripts'])
         os.system(scp1)
  
 def sshAmbariServer() :
     ssh1=''.join(['scp',' ', '-p', ' ', '-i', PEM_FILE, ' ', ssl_script, ' ', host_file, ' ', 'root', '@', AMBARI_DOMAIN, ':', '/tmp'])
     os.system(ssh1)
-    ssh2=''.join(['ssh',' ', 'root', '@', AMBARI_DOMAIN, ' ' ,'-i', PEM_FILE, ' ', '/tmp/ssl_create.sh'])
+    ssh2=''.join(['ssh',' ', 'root', '@', AMBARI_DOMAIN, ' ' ,'-i', PEM_FILE, ' ', '/tmp/ssl_create.sh setup'])
     os.system(ssh2)
-    test=True
-    while test :
-       sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       result = sock.connect_ex((AMBARI_DOMAIN, int(AMBARI_PORT)))
-       #print "Waiting for Ambari server to start"
-       if result == 0:
-           #print "\nAmbari port 8080 is UP now proceeding with further tasks"
-           test=False
-    #s='.'
-    #sys.stdout.write( s )
-    #sys.stdout.flush()
-    time.sleep(2)
-    sock.close()
-    
-    
+
 history_server=getMRHistory()
 
-#def clean_up():
+def clean_up():
+    ssh2=''.join(['ssh',' ', 'root', '@', AMBARI_DOMAIN, ' ' ,'-i', PEM_FILE, ' ', '/tmp/ssl_create.sh kill'])
+    os.system(ssh2)
+    os.remove(ssl_script)
+    os.remove(host_file)
+
 
 def requestCopyKeystore() :
-    #url="http://"+AMBARI_DOMAIN+":"+str(AMBARI_PORT)+restAPI
+
     url = "http://"+AMBARI_DOMAIN+":"+str(AMBARI_PORT)+CLUSTER_API+"/requests"
-    data = {"RequestInfo":{"context":"Execute an action", "action" : "ssl_setup", "service_name" : "", "component_name":"", "hosts":" "}}
+    data = {"RequestInfo":{"context":"Execute an action", "action" : "validate_configs", "service_name" : "", "component_name":"", "hosts":" "}}
     data1= json.dumps(data)
     request = urllib2.Request(url)
     base64string = base64.encodestring('%s:%s' % (AMBARI_USER_ID, AMBARI_USER_PW)).replace('\n', '')
     request.add_header("Authorization", "Basic %s" % base64string)
     request.add_header('X-Requested-By', 'ambari')
     request.add_data(data1)
-    #print request.data
     result = urllib2.urlopen(request)
-    #return(json.load(result))
+
     
 def restartRequired(): 
     url = "http://"+AMBARI_DOMAIN+":"+str(AMBARI_PORT)+CLUSTER_API+"/requests"
@@ -234,18 +210,12 @@ def restartRequired():
 
 
 def changeConfig(config_type,key,value):
-  #change_config_main() 
-  #print "Changing config for %s" % config_type
-  host=AMBARI_DOMAIN
-  login=AMBARI_USER_PW
-  password=AMBARI_USER_ID
-  port=AMBARI_PORT
 
-  def api_accessor(host, login, password, protocol, port):
+  def api_accessor(AMBARI_DOMAIN, AMBARI_USER_ID, AMBARI_USER_PW, protocol, AMBARI_PORT):
     def do_request(api_url, request_type=GET_REQUEST_TYPE, request_body=''):
      try:
-      url = '{0}://{1}:{2}{3}'.format(protocol, host, port, api_url)
-      admin_auth = base64.encodestring('%s:%s' % (login, password)).replace('\n', '')
+      url = '{0}://{1}:{2}{3}'.format(protocol, AMBARI_DOMAIN, AMBARI_PORT, api_url)
+      admin_auth = base64.encodestring('%s:%s' % (AMBARI_USER_ID, AMBARI_USER_PW)).replace('\n', '')
       request = urllib2.Request(url)
       request.add_header('Authorization', 'Basic %s' % admin_auth)
       request.add_header('X-Requested-By', 'ambari')
@@ -275,15 +245,16 @@ def changeConfig(config_type,key,value):
       new_config[CLUSTERS][DESIRED_CONFIGS][ATTRIBUTES] = attributes
     request_body = json.dumps(new_config)
     new_file = 'doSet_{0}.json'.format(new_tag)
-    logger.info('### PUTting json into: {0}'.format(new_file))
+    logging.info('### PUTting json into: {0}'.format(new_file))
     output_to_file(new_file)(new_config)
     accessor(CLUSTER_API.format(cluster), PUT_REQUEST_TYPE, request_body)
-    logger.info('### NEW Site:{0}, Tag:{1}'.format(config_type, new_tag))
+    logging.info('### NEW Site:{0}, Tag:{1}'.format(config_type, new_tag))
+    os.remove(new_file)
 
 
   def get_current_config(cluster, config_type, accessor):
     config_tag = get_config_tag(cluster, config_type, accessor)
-    logger.info("### on (Site:{0}, Tag:{1})".format(config_type, config_tag))
+    logging.info("### on (Site:{0}, Tag:{1})".format(config_type, config_tag))
     response = accessor(CONFIGURATION_URL.format(cluster, config_type, config_tag))
     config_by_tag = json.loads(response)
     current_config = config_by_tag[ITEMS][0]
@@ -298,7 +269,7 @@ def changeConfig(config_type,key,value):
 
 
   def set_properties(cluster, config_type, args, accessor):
-    logger.info('### Performing "set":')
+    logging.info('### Performing "set":')
 
     if len(args) == 1:
       config_file = args[0]
@@ -308,14 +279,14 @@ def changeConfig(config_type,key,value):
       elif ext == ".json":
         updater = update_from_file(config_file)
       else:
-        logger.error("File extension {0} doesn't supported".format(ext))
+         logging.error("File extension {0} doesn't supported".format(ext))
         return -1
-      logger.info('### from file {0}'.format(config_file))
+       logging.info('### from file {0}'.format(config_file))
     else:
       config_name = args[0]
       config_value = args[1]
       updater = update_specific_property(config_name, config_value)
-      logger.info('### new property - "{0}":"{1}"'.format(config_name, config_value))
+      logging.info('### new property - "{0}":"{1}"'.format(config_name, config_value))
     update_config(cluster, config_type, updater, accessor)
     return 0
   def update_config(cluster, config_type, config_updater, accessor):
@@ -323,19 +294,9 @@ def changeConfig(config_type,key,value):
     create_new_desired_config(cluster, config_type, properties, attributes, accessor)
 
   def change_config_main():
-    user = AMBARI_USER_ID
-    password = AMBARI_USER_PW
-    #port = 8080
-    #protocol = 'http'
-
     action = 'set'
-    host = AMBARI_DOMAIN
     cluster = CLUSTER
-    #config_type = sys.argv[4]
-    #key=sys.argv[5]
-    #value=sys.argv[6]
-    #print "IN MAIN"
-    accessor = api_accessor(host, user, password, protocol, AMBARI_PORT)
+    accessor = api_accessor(AMBARI_DOMAIN, AMBARI_USER_ID, AMBARI_USER_PW, protocol, AMBARI_PORT)
     if action == SET_ACTION:
       action_args = [key, value]
       return set_properties(cluster, config_type, action_args, accessor)
@@ -371,33 +332,30 @@ def changeConfig(config_type,key,value):
 history_server_url="https://"+history_server+":"+"19890"+"/jobhistory/logs"
 
 
+logging.info('Creating the hosts.txt file')
 getAmbariHostName()
+
+logging.info('Execute Ambari server prepare')
 sshAmbariServer()
 
+logging.info('Execute Ambari agent prepare')
+sshAmbariAgents() 
 
-def sslAgentWait() :
-  restAPI = CLUSTER_API+"/hosts?fields=Hosts/host_state"
-  json_data =  ambariREST(restAPI)
-  for index in range(len(json_data['items'])):
-    host_state=json_data['items'] [index] ['Hosts'] ['host_state']
-    if host_state is 'HEARTBEAT_LOST' :
-        index = 0
-        time.sleep(2)
-  sshAmbariAgents()     
-  requestCopyKeystore()
 
-sslAgentWait()
+logging.info('Execute Ambari API to copy keystore on all agents')    
+requestCopyKeystore()
+
+
 changeConfig('hdfs-site','dfs.http.policy','HTTPS_ONLY')
 changeConfig('ssl-client','ssl.client.keystore.password','changeit')
 changeConfig('ssl-client','ssl.client.truststore.password','changeit')
 changeConfig('ssl-server','ssl.server.truststore.password','changeit')
 changeConfig('ssl-server','ssl.server.keystore.password','changeit')
 changeConfig('ssl-server','ssl.server.keystore.keypassword','changeit')
-changeConfig('yarn-site','yarn.log.server.url','HTTPS_ONLY')
+changeConfig('yarn-site','yarn.http.policy','HTTPS_ONLY')
 changeConfig('yarn-site','yarn.log.server.url',history_server_url)
 changeConfig('mapred-site','mapreduce.jobhistory.http.policy','HTTPS_ONLY')
 changeConfig('mapred-site','mapreduce.jobhistory.webapp.https.address','0.0.0.0:19890')
+
+logging.info('Execute Ambari API to restart all required services') 
 restartRequired()
-
-
-
